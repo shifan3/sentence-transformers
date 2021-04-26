f5e23786835f2e98cea5651e20ab9352f34ab27f
shi.fan <shi.fan@danatech.com>
2021-04-26 17:13:54 +0800

    asdsa

--- train.sh
+++ train.sh
@@ -1,4 +1,5 @@
 python3.7 train_supervised_text_embedding.py \
     --train-file $GEMINI_DATA_IN/sbert.small/sbert.train.txt \
     --eval-file $GEMINI_DATA_IN/sbert.small/sbert.eval.txt \
+    --epoch 5 \
     --batch-size 32 |&tee train.log 
