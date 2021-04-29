5351d25eb10230a41a88b21f241b37fbe47a51ec
shi.fan <shi.fan@danatech.com>
2021-04-29 22:39:29 +0800

    asda

--- train.sh
+++ train.sh
@@ -3,4 +3,5 @@ python3.7 train.py \
     --eval-file $GEMINI_DATA_IN/sbert.small/sbert.eval.txt \
     --output-dir $GEMINI_DATA_OUT/
     --epoch 5 \
-    --batch-size 32 |&tee train.log 
+    --eval-log-file $GEMINI_DATA_OUT/eval.log \
+    --batch-size 32 > train.log 2>&1
