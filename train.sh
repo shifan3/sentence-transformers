2af5a0bcd9b7264aa1bd1afc4a5405c948dd0684
shi.fan <shi.fan@danatech.com>
2021-04-30 00:06:02 +0800

    zdzsd

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 python3.7 train.py \
     --train-file sbert.train.txt \
     --eval-file sbert.eval.txt \
-    --output-dir $GEMINI_DATA_OUT/
+    --output-dir $GEMINI_DATA_OUT/ \
     --epoch 5 \
     --eval-log-file $GEMINI_DATA_OUT/eval.log \
     --batch-size 32 > train.log 2>&1
