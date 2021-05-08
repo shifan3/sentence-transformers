7693e1b6db29a54644a711ce1bcf32b4fac59485
shi.fan <shi.fan@danatech.com>
2021-05-08 20:54:29 +0800

    ad

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 python3.7 train.py \
-    --train-file $GEMINI_DATA_IN/sbert.1/train.txt \
-    --eval-file $GEMINI_DATA_IN/sbert.1/eval.txt \
+    --train-file $GEMINI_DATA_IN/sbert.2/train.txt \
+    --eval-file $GEMINI_DATA_IN/sbert.2/eval.txt \
     --output-dir $GEMINI_DATA_OUT/ \
     --epoch 10 \
     --eval-log-file eval.log \
